33025e5b4bca88638899e994bef4f43350dea510
anhndt-miu <dangtuananh.nguyen@miu.edu>
2024-10-04 13:35:49 -0500

    feat(Lab4) (#3)
    * add lab3
    
    * Add lab 4
    
    ---------
    
    Co-authored-by: Anh Nguyễn <kzjn10@users.noreply.github.com>

---
+++ Lab4/lab4.js
@@ -0,0 +1,63 @@
+// var foo = 1;
+//
+// function bar() {
+//     if (!foo) {
+//         var foo = 10
+//     }
+//
+//     console.log(foo)
+// }
+//
+// bar()
+// console.log(foo)
+// 3. 10
+
+
+var x = 9
+
+function myFunction() {
+    return x * x
+}
+
+console.log(myFunction())
+x = 5
+console.log(myFunction())
+// console.log("------------")
+
+// 2. 81 25
+
+///-----------------
+// let x;
+// var a = 5;
+// var b = 10;
+// var c = function (a, b, c) {
+//     console.log(x)
+//     console.log(a)
+//     var f = function (a, b, c) {
+//         b = a
+//         console.log(b)
+//         b = c
+//         var x = 5
+//     }
+//
+//     f(a, b, c)
+//     console.log(b)
+//      var x = 10
+//
+// }
+//
+// c(8, 9, 10)
+// console.log(b)
+// console.log(x)
+
+// undefine
+// 8
+// 8
+// 9
+//10
+//undefine
+
+
+
+
+
