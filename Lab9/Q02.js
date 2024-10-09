33d0f9301fd541215b97d92d696072978fa2e675
Anh Nguyễn <kzjn10@users.noreply.github.com>
2024-10-09 14:56:40 -0500

    update Lab9

---
+++ Lab9/Q02.js
@@ -0,0 +1,8 @@
+
+Question2: Create a web server using http module:
+
+    If the path is ‘/image’ and method is GET, send a response of an image to any client. Set an appropriate content-type for your image.
+If the path is ‘/pdf’ and method is GET, send a response of a pdf file to any client. Set a Content-type as "application/pdf".
+If the path is ‘/about’ and method is GET, send a response of a txt file to any client. Set a Content-type as "text/plain".
+If the path is ‘/home’ or ‘/’ and method is GET, send “Welcome to my website” text.
+    Otherwise return not found with status code 404.
